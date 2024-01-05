cc6aa5de498bb0e285f7aec47af9ee31ca1aafa3
Clifton Patterson <Cliftonp15@yahoo.com>
2024-01-05 11:20:55 -0500

    Intro & Activating Strict Mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+let hasDriversLicense = true;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Audio'; // cant use because its a keyword
+//const private = 534; // cant use because its a keyword
+//const if = "this doesnt work either because its a keyword";
+
+
