c8f3f1d6221b6be5a5b1b47f8e7153c355c935a8
Clifton Patterson <Cliftonp15@yahoo.com>
2024-01-05 13:24:31 -0500

    Arrow functions - single line and block

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -67,3 +67,29 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1994);
 
 console.log(age1, age2)
+
+//------- Arrow Functions -------//
+
+//Removes need to use block (this works due to it being single line)
+//or return happens implicitly without needing to put return 
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);// returned value the same as calcAge2 even  without the return keyword
+
+//Since this is kmore than one line of code we need to use code block here
+const yearsUntilRetirement = birthYear => {
+    const age = 2037 - birthYear;
+    const retirement  = 65 - age;
+    return retirement;
+}
+console.log(yearsUntilRetirement(1991))
+
+//Same function Using multiple parameters
+//When using multiple parameters use parenthesis 
+const yearsUntilRetirement2 = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement  = 65 - age;
+    return `${firstName}  retires in ${retirement} years.`;
+}
+
+console.log(yearsUntilRetirement2(1991, 'cliff'));
